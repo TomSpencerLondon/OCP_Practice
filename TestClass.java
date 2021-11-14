311a794c740e21acbf085586f82edf461342a2b4
Tom <thomas.spencer@codurance.com>
2021-11-14 16:19:49 +0000

    add

---
+++ TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+        // Add test code here
+        
+
+        // Add any print statements here
+        System.out.println("Hello world!");
+    }
+}
